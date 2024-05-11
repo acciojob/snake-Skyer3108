17911c72a8ec30b658d59ff9305e6c52dfad25b8
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-05-11 21:20:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
 //your code here
+const gameContainer = document.getElementById('gameContainer');
+const scoreDisplay = document.getElementById('score');
+const gridSize = 40;
+let snake = [{x: 20, y: 1}];
+let direction = 'right';
+let food = {};
+let score = 0;
 
-const gameContainer=document.getElementById('gameContainer')
-const scoreDisplay=document.getElementById('score')
-
-const gridSize=40;
-let snake=[{x:20,y:1}]
-let direction='right';
-let food={}
-let score=0;
-
-
-function createPixel(x,y,className){
-
-	const pixel=document.createElement('div');
-
-	pixel.classList.add('pixel',className);
-
-	pixel.style.gridColumn=x;
-	pixel.style.gridRow=y
-
-	gameContainer.appendChild(pixel)
+function createPixel(x, y, className) {
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel', className);
+    pixel.style.gridColumn = x;
+    pixel.style.gridRow = y;
+    gameContainer.appendChild(pixel);
 }
-
 
 function createFood() {
     let x = Math.floor(Math.random() * gridSize) + 1;
@@ -101,5 +93,3 @@ const gameInterval = setInterval(() => {
     drawSnake();
     checkCollision();
 }, 100);
-
-
